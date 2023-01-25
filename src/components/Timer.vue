e420c7b633467c26b6fec64b0cd9735102b8a86a
Kevin Tatooles <tatooles@wisc.edu>
2023-01-24 20:27:30 -0600

    Add mock buttons

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -3,5 +3,11 @@
 </script>
 
 <template>
-  <div class="text-7xl flex flex-col h-60 justify-center">00:00.00</div>
+  <div class="flex flex-col h-60 justify-center">
+    <h1 class="text-7xl">00:00.00</h1>
+    <div class="flex flex-row justify-center mt-4">
+      <button class="mr-4 border-white border-2 rounded-md p-2">Start</button>
+      <button class="border-white border-2 rounded-md p-2">Stop</button>
+    </div>
+  </div>
 </template>
