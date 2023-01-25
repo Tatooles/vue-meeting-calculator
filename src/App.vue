4b7396012b0655f5e362802ba07a072138868e06
Kevin Tatooles <tatooles@wisc.edu>
2023-01-24 20:19:46 -0600

    Mocking out layout

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
+import ParticipantList from './components/ParticipantList.vue';
+
 </script>
 
 <template>
-  <header>
-    <h1>Meeting Cost Calculator</h1>
-  </header>
+  <div class="flex flex-col text-center bg-zinc-900 h-screen text-white w-full md:w-3/4 mx-auto p-5">
+    <header>
+      <h1 class="text-2xl md:text-4xl font-bold">Meeting Cost Calculator</h1>
+    </header>
 
-  <main>
-  </main>
+    <main class="p-5">
+      <ParticipantList />
+    </main>
+
+    <footer class="text-xs bottom-3 absolute left-1/2 translate-x-[-50%]">
+      Created by Kevin Tatooles
+    </footer>
+  </div>
 </template>
