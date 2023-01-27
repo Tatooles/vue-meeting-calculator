bdfc3433ef6f288575382c5c6eb2f877b565f523
Kevin Tatooles <tatooles@wisc.edu>
2023-01-26 21:39:36 -0600

    Improve input styling

--- src/components/TimeInput.vue
+++ src/components/TimeInput.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+
+</script>
+
 <template>
-  <input type="text" class="border-2 border-white bg-inherit">
+  <div class="bg-inherit h-1/2 w-full flex justify-center">
+    <input type="text" placeholder="HH:MM:SS"
+      class=" text-center text-4xl border-2 border-white h-full w-3/4 bg-inherit">
+  </div>
 </template>
