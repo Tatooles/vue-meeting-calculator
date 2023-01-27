d8ea028686a9c403f0639d66b499f340e74216e9
Kevin Tatooles <tatooles@wisc.edu>
2023-01-27 17:21:56 -0600

    Restyle and add button functions

--- src/components/ParticipantList.vue
+++ src/components/ParticipantList.vue
@@ -1,22 +1,42 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
-const participants = ref([
-  { role: 'Software Engineer', salary: '100,000' }
+const roles = ref([
+  { name: 'Software Engineer', salary: '100,000', count: 1 }
 ]);
 
-participants.value.push({ role: 'Manager', salary: '120,000' });
+roles.value.push({ name: 'Manager', salary: '120,000', count: 1 });
+
+const increaseCount = (role: any) => {
+  console.log(`Increasing count of ${role.name}`);
+}
+
+const decreaseCount = (role: any) => {
+  console.log(`Decreasing count of ${role.name}`);
+}
 
 </script>
 
 <template>
   <ul>
-    <template v-for="participant in participants">
-      <li>
-        {{ participant.role }}, ${{ participant.salary }}
+    <template v-for="role in roles">
+      <!-- TODO: Also need a way to remove a role from the list -->
+      <li class="border-2 border-white mb-2 flex justify-between p-2">
+        <div>
+          {{ role.name }}
+        </div>
+        <div>
+          ${{ role.salary }}
+        </div>
+        <div>
+          <button @click="decreaseCount(role)">-</button>
+          {{ role.count }}
+          <!-- TODO: Also want to be able to directly enter a value for this -->
+          <button @click="increaseCount(role)">+</button>
+        </div>
       </li>
 
     </template>
-    <button>+ Add User</button>
+    <button class="mb-2">+ Add Role</button>
   </ul>
 </template>
