1632cbc86412f724e0db7d13c4872a6b81d146ef
Kevin Tatooles <tatooles@wisc.edu>
2023-01-30 22:18:22 -0600

    Add validation for the salary

--- src/components/ParticipantList.vue
+++ src/components/ParticipantList.vue
@@ -9,7 +9,7 @@ const modalOpen = ref(false);
 
 const newRoleName = ref('');
 const newRoleSalary = ref('');
-const salaryValidationError = ref(true);
+const salaryValidationError = ref(false);
 
 roles.value.push({ name: 'Manager', salary: 120000, count: 1 });
 
@@ -29,17 +29,25 @@ const decreaseCount = (role: any) => {
 
 const closeModal = (save: boolean) => {
   if (save) {
-    // Save the data
-    if (isNaN(parseInt(newRoleSalary.value))) {
-      console.log('Please enter a valid number');
+    if (newRoleName.value.length === 0 || newRoleSalary.value.length === 0) {
+      // TODO: Could add more validation here
       return;
     }
+    // Save the data
     roles.value.push({ name: newRoleName.value, salary: parseInt(newRoleSalary.value), count: 1 });
   }
   modalOpen.value = false;
   newRoleName.value = '';
   newRoleSalary.value = '';
   // Clear the fields
+}
+
+const validateSalary = () => {
+  if (newRoleSalary.value.length !== 0 && isNaN(parseInt(newRoleSalary.value))) {
+    salaryValidationError.value = true;
+  } else {
+    salaryValidationError.value = false;
+  }
 }
 
 </script>
@@ -73,9 +81,11 @@ const closeModal = (save: boolean) => {
       <h1 class="text-2xl mb-2">Add a New Role</h1>
       <form action="">
         <input v-model="newRoleName" type="text" placeholder="Role Name" class="mb-2 p-1 border-2">
-        <input v-model="newRoleSalary" type="text" placeholder="Role Salary" class="mb-2 p-1 border-2">
+        <input v-model="newRoleSalary" @input="validateSalary" type="text" placeholder="Role Salary"
+          class="p-1 border-2">
+        <p v-if="salaryValidationError" class="text-sm text-red-500">Please enter a valid number</p>
       </form>
-      <div>
+      <div class="mt-2">
         <button @click="closeModal(false)" class="p-2 border-black rounded-lg">Cancel</button>
         <button @click="closeModal(true)"
           class="p-2 border-black bg-blue-600 rounded-lg text-white ml-4">Submit</button>
