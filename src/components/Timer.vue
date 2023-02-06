be9cb08369a8d2fe33fd38896532056cd2537a7a
Kevin Tatooles <tatooles@wisc.edu>
2023-02-05 19:16:23 -0600

    Implement simple version of a timer

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,7 +1,25 @@
+<script setup lang="ts">
+import { useStopwatch } from "vue-timer-hook";
+// const autoStart = false;
+const stopwatch = useStopwatch(0, false);
+</script>
+
 <template>
-  <h1 class="text-5xl">00:00.00</h1>
+  <h1 class="text-5xl">
+    {{ stopwatch.hours }}:{{ stopwatch.minutes }}:{{ stopwatch.seconds }}
+  </h1>
   <div class="mt-4 flex flex-row justify-center">
-    <button class="mr-4 rounded-md border-2 border-white p-2">Start</button>
-    <button class="rounded-md border-2 border-white p-2">Stop</button>
+    <button
+      @click="stopwatch.start"
+      class="mr-4 rounded-md border-2 border-white p-2"
+    >
+      Start
+    </button>
+    <button
+      @click="stopwatch.pause"
+      class="rounded-md border-2 border-white p-2"
+    >
+      Stop
+    </button>
   </div>
 </template>
