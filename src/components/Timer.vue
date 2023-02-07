db9812d5d122361bddcd8c775f15e868ee78693c
Kevin Tatooles <tatooles@wisc.edu>
2023-02-06 21:20:35 -0600

    Display time correctly

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,12 +1,37 @@
 <script setup lang="ts">
 import { useStopwatch } from "vue-timer-hook";
-// const autoStart = false;
+import { computed } from "vue";
+
 const stopwatch = useStopwatch(0, false);
+
+// Computed values so we always have a 2 digit number
+const hourLeft = computed(() => {
+  return Math.floor(stopwatch.hours.value / 10);
+});
+const hourRight = computed(() => {
+  return stopwatch.hours.value % 10;
+});
+
+const minuteLeft = computed(() => {
+  return Math.floor(stopwatch.minutes.value / 10);
+});
+const minuteRight = computed(() => {
+  return stopwatch.minutes.value % 10;
+});
+
+const secondLeft = computed(() => {
+  return Math.floor(stopwatch.seconds.value / 10);
+});
+const secondRight = computed(() => {
+  return stopwatch.seconds.value % 10;
+});
 </script>
 
 <template>
   <h1 class="text-5xl">
-    {{ stopwatch.hours }}:{{ stopwatch.minutes }}:{{ stopwatch.seconds }}
+    {{ hourLeft }}{{ hourRight }}:{{ minuteLeft }}{{ minuteRight }}:{{
+      secondLeft
+    }}{{ secondRight }}
   </h1>
   <div class="mt-4 flex flex-row justify-center">
     <button
