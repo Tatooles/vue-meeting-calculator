51e676b9cbfd2e16e6a8c8d4d4338c17443f4593
Kevin Tatooles <tatooles@wisc.edu>
2023-02-09 22:34:32 -0600

    Fix bug and give visual feedback on timer

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,9 +1,24 @@
 <script setup lang="ts">
 import { useStopwatch } from "vue-timer-hook";
-import { computed, watch } from "vue";
+import { computed, watch, ref } from "vue";
 import { store } from "../store.js";
 
 const stopwatch = useStopwatch(0, false);
+
+const running = ref(false);
+
+const startTime = () => {
+  stopwatch.start;
+  console.log("called start");
+  console.log(stopwatch.isRunning.value);
+  running.value = true;
+};
+
+const stopTime = () => {
+  stopwatch.pause;
+  console.log("called stop");
+  running.value = false;
+};
 
 watch(stopwatch.seconds, async () => {
   store.setTimerSeconds(stopwatch.seconds);
@@ -36,6 +51,7 @@ const secondLeft = computed(() => {
   return Math.floor(stopwatch.seconds.value / 10);
 });
 const secondRight = computed(() => {
+  console.log("computing second");
   return stopwatch.seconds.value % 10;
 });
 </script>
@@ -47,15 +63,31 @@ const secondRight = computed(() => {
     }}{{ secondRight }}
   </h1>
   <div class="mt-4 flex flex-row justify-center">
+    <!-- TODO: It's buggy af when they press start multiple times, prevent that behavior -->
     <button
+      v-show="!stopwatch.isRunning.value"
       @click="stopwatch.start"
       class="mr-4 rounded-md border-2 border-white p-2"
     >
       Start
     </button>
     <button
+      v-show="stopwatch.isRunning.value"
+      class="border-whit mr-4 rounded-md border-2 p-2 shadow-inner shadow-white"
+    >
+      Start
+    </button>
+
+    <button
+      v-show="stopwatch.isRunning.value"
       @click="stopwatch.pause"
       class="rounded-md border-2 border-white p-2"
+    >
+      Stop
+    </button>
+    <button
+      v-show="!stopwatch.isRunning.value"
+      class="rounded-md border-2 border-white p-2 shadow-inner shadow-white"
     >
       Stop
     </button>
